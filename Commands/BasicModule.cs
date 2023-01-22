251554fbb52e155c662025926a9a988299813496
KoenBoetius <45605528+Koin101@users.noreply.github.com>
2023-01-22 12:32:42 +0100

    !5stack updated

--- Commands/BasicModule.cs
+++ Commands/BasicModule.cs
@@ -100,40 +100,45 @@ namespace Discord_Bot.Commands
         [Command("5Stack"), Description("This command will ping everyone with the @league tag who isn't already in the voice channel.")]
         public async Task ping5Stack(CommandContext ctx)
         {
+            if (ctx.Member.VoiceState == null || ctx.Member.VoiceState.Channel == null)
+            {
+                await ctx.RespondAsync("You are not in a voiceChannel");
+                return;
+            }
             DiscordRole LeagueRole = ctx.Guild.GetRole(828775478231040031);
             var LeagueUsers = ctx.Guild.Members.Where(user => user.Value.Roles.Contains(LeagueRole));
             var LeagueUsersDict = LeagueUsers.ToDictionary(i => i.Key, i => i.Value);
-            var ChannelUsers = ctx.Guild.VoiceStates[173016823883628545].Channel.Users;
+            var ChannelUsers = ctx.Member.VoiceState.Channel.Users;
             
             StringBuilder stringBuilder = new StringBuilder();
-
-            stringBuilder.Append("Hey! ");
+           
+            stringBuilder.Append("To those who do not touchgrass! \n");
             int i = 1;
-
+            var downArrow = DiscordEmoji.FromName(ctx.Client, ":arrow_down:");
             foreach (var channelUser in ChannelUsers) LeagueUsersDict.Remove(channelUser.Id);            
 
             foreach (var user in LeagueUsersDict.Values)
-            {              
+            {
+                
+                stringBuilder.Append("- ");
                 stringBuilder.Append(user.Mention);
-                if (i < LeagueUsersDict.Count()) stringBuilder.Append(", ");
-                else stringBuilder.Append(": \n");
-                i++;
+                stringBuilder.Append("\n");
+         
             }
-            i = 1;
+            for (int j = 0; j < 10; j++)
+            {
+                stringBuilder.Append(downArrow + "\t\t");
+            }
+            stringBuilder.Append("\n");
             foreach (var user in ChannelUsers) 
-            { 
+            {
 
+                stringBuilder.Append("- ");
                 stringBuilder.Append(user.Mention);
-                if (i < ChannelUsers.Count)
-                {
-                    stringBuilder.Append(", ");
-                }
-                else
-                    stringBuilder.Append(": ");
-                i++;
+                stringBuilder.Append("\n");
             }
 
-            stringBuilder.Append("\nReally want to play League with a 5 stack. Join them or ur gay");
+            stringBuilder.Append("Really want to play League with a 5 stack. Join them or ur gay");
 
             await ctx.RespondAsync(stringBuilder.ToString());
 
