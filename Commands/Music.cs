16b53d16cf64e587c5272c97381001024d3deb4a
Koin101 <45605528+Koin101@users.noreply.github.com>
2023-03-26 00:29:39 +0100

    basic music done?

--- Commands/Music.cs
+++ Commands/Music.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Discord;
-using Discord.Interactions;
+
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -17,7 +16,7 @@ namespace Discord_Bot.Commands
     {
 
         [Command]
-        public async Task Join(CommandContext ctx)
+        public async Task Join(CommandContext ctx, DiscordChannel channel)
         {
             var lava = ctx.Client.GetLavalink();
             if (!lava.ConnectedNodes.Any())
@@ -28,10 +27,107 @@ namespace Discord_Bot.Commands
 
             var node = lava.ConnectedNodes.Values.First();
 
-            if(ctx.Member.VoiceState.Channel == null)
+            if (channel.Type != ChannelType.Voice)
             {
-
+                await ctx.RespondAsync("Not a valid voice channel!");
+                return;
             }
+
+            await node.ConnectAsync(channel);
+            await ctx.RespondAsync($"Joined {channel.Name}");
+        }
+
+        [Command]
+        public async Task Leave(CommandContext ctx, DiscordChannel channel)
+        {
+            var lava = ctx.Client.GetLavalink();
+            if (!lava.ConnectedNodes.Any())
+            {
+                await ctx.RespondAsync("A connection could not be established, please contact auke");
+                return;
+            }
+
+            var node = lava.ConnectedNodes.Values.First();
+
+            if (channel.Type != ChannelType.Voice)
+            {
+                await ctx.RespondAsync("Not a valid voice channel.");
+                return;
+            }
+
+            var conn = node.GetGuildConnection(channel.Guild);
+
+            if (conn == null)
+            {
+                await ctx.RespondAsync("Lavalink is not connected.");
+                return;
+            }
+
+            await conn.DisconnectAsync();
+            await ctx.RespondAsync($"Left {channel.Name}!");
+        }
+
+        [Command]
+        public async Task Play(CommandContext ctx, [RemainingText] string search)
+        {
+            if (ctx.Member.VoiceState == null || ctx.Member.VoiceState.Channel == null)
+            {
+                await ctx.RespondAsync("You are not in a voice channel retard.");
+                return;
+            }
+
+            var lava = ctx.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+
+            if (conn == null)
+            {
+                await ctx.RespondAsync("Lavalink is not connected, blame Auke");
+                return;
+            }
+
+            var loadResult = await node.Rest.GetTracksAsync(search);
+
+            if (loadResult.LoadResultType == LavalinkLoadResultType.LoadFailed || loadResult.LoadResultType == LavalinkLoadResultType.NoMatches)
+            {
+                await ctx.RespondAsync($"Track search failed for {search}. Try better or blame Auke.");
+                return;
+            }
+
+            var track = loadResult.Tracks.First();
+
+            await conn.PlayAsync(track);
+
+            await ctx.RespondAsync($"Now playing {track.Title}!");
+        }
+
+        [Command]
+        public async Task Pause(CommandContext ctx)
+        {
+            if (ctx.Member.VoiceState == null || ctx.Member.VoiceState.Channel == null)
+            {
+                await ctx.RespondAsync("You are not in a voice channel.");
+                return;
+            }
+
+            var lava = ctx.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+
+            if (conn == null)
+            {
+                await ctx.RespondAsync("Lavalink is not connected.");
+                return;
+            }
+
+            if (conn.CurrentState.CurrentTrack == null)
+            {
+                await ctx.RespondAsync("There are no tracks loaded.");
+                return;
+            }
+
+            await conn.PauseAsync();
+
         }
     }
 }
